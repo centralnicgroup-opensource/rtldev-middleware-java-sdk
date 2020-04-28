77c092a04622ebd9e2d7188aa0667d3db31e8c33
Kai Schwarz <kschwarz@hexonet.net>
2020-04-28 12:49:17 +0200

    feat(response): added getCommandPlain (getting used command in plain text)

--- src/test/java/net/hexonet/apiconnector/ResponseTest.java
+++ src/test/java/net/hexonet/apiconnector/ResponseTest.java
@@ -15,6 +15,23 @@ import org.junit.Test;
  * Unit test for class Response
  */
 public class ResponseTest {
+
+    /**
+     * Test getCommandPlain method
+     */
+    @Test
+    public void getCommandPlain() {
+        // ensure no vars are returned in response, just in case no place holder replacements are
+        // provided
+        Map<String, String> cmd = new HashMap<String, String>();
+        cmd.put("COMMAND", "QueryDomainOptions");
+        cmd.put("DOMAIN0", "example.com");
+        cmd.put("DOMAIN1", "example.net");
+        Response r = new Response("", cmd);
+        String str = "COMMAND = QueryDomainOptions\nDOMAIN0 = example.com\nDOMAIN1 = example.net\n";
+        assertEquals(str, r.getCommandPlain());
+    }
+
     /**
      * Test getCurrentPageNumber method #1
      */
